e225ab0e9ad079779c300904c2d77401621f9ffa
tenraneko <nekocubot@gmail.com>
2022-10-03 23:28:06 +0600

    added writing line to file

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,3 @@
-import { privateEncrypt } from "crypto";
 import * as vscode from "vscode";
 import * as fs from "fs";
 
@@ -7,32 +6,74 @@ export function activate(context: vscode.ExtensionContext) {
     const eventFilePath = vscode.window.activeTextEditor?.document.fileName;
 
     //* Wrong file selected
-    if (eventFilePath && eventFilePath!.indexOf("_event.dart") < 0) {
+    if (!eventFilePath || (eventFilePath!.indexOf("_event.dart") < 0 && eventFilePath!.indexOf("_events.dart") < 0)) {
       vscode.window.showInformationMessage("Error! Wrong file.");
       return;
     }
 
     //* Bloc file exists
-    const blocFilePath = eventFilePath?.replace("_event.dart", "_bloc.dart");
+    const blocFilePath = eventFilePath?.replace(/_event.dart|_events.dart/, "_bloc.dart");
     if (!blocFilePath) {
       vscode.window.showInformationMessage("Couldn't find sing up to the right files");
       return;
     }
 
+    var blocTextCode: string = await fs.readFileSync(blocFilePath, "utf-8");
+    if (!blocTextCode) {
+      return;
+    }
+
     //* Events text code
     const eventsTextCode = vscode.window.activeTextEditor?.document.getText();
-    const mainEventName = eventsTextCode?.match(/abstract class (\w*)/)!.at(1);
+    const mainEventName = eventsTextCode?.match(/abstract class (\w*)/)?.at(1);
+    const mainStateName = blocTextCode?.match(/extends Bloc\<\w+,\s(\w+)/)?.at(1);
 
     if (mainEventName) {
-      const events = eventsTextCode?.match(/^class (\w+)/gm)?.map((e) => e.replace("class ", ""));
+      const events = eventsTextCode?.match(/(?<=^\sclass )[^&]\w*/gm);
 
       if (events) {
-        const blocTextCode = await fs.readFileSync(blocFilePath, "utf-8");
+        const blocEvents = blocTextCode!.replace(/(?<=\son<)[^&].*(?=>\(\w*\)\;)/gm, "");
+        const blocCodeLines = blocTextCode.split("\n");
+        let startFounded = false;
+        let endBlocIndex;
+        let endFileIndex;
 
-        if (blocTextCode) {
-          const blocEvents = blocTextCode?.match(/on<\w+>\(\w+\);/gm)?.map((e) => e.replace(/on\<(\w+).*/, "$2"));
+        for (let index = blocCodeLines.length - 1; index > 0; index--) {
+          const line = blocCodeLines[index];
+          if (line?.trim() == "}") {
+            endFileIndex = index + 1;
+            break;
+          }
+        }
 
-          console.log(blocEvents);
+        for (let i = 0; i < blocCodeLines.length; i++) {
+          const e = blocCodeLines[i];
+          if (startFounded == false) {
+            if (e.match(/\.*class\s.*extends\s*Bloc\<\w*,\s\w*\>\s*{/)) {
+              startFounded = true;
+            }
+          } else {
+            if (e?.trim() == "}") {
+              endBlocIndex = i;
+              break;
+            }
+          }
+        }
+
+        if (endBlocIndex && endFileIndex) {
+          endFileIndex--;
+          for (let index = 0; index < events.length; index++) {
+            const event = events[index];
+            if (blocEvents?.includes(event) == false) {
+              const functionName = `_${event[0].toLowerCase()}${event.slice(1)}`;
+              blocCodeLines.splice(endBlocIndex, 0, `\t\ton<${event}>(${functionName});`);
+              endFileIndex++;
+              blocCodeLines.splice(endFileIndex, 0, `\n\tFuture<void> ${functionName}(${event} event, Emitter<${mainStateName}> emit) async {}`);
+              endFileIndex++;
+              endBlocIndex++;
+            }
+          }
+          fs.writeFileSync(blocFilePath, blocCodeLines.join("\n"));
         }
       }
     }
